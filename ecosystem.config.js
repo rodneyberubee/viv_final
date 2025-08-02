6c021e10265dbdb72e7ab0579d705a9479f818f2
rodneyberubee <rodney@vivaitable.com>
2025-08-02 09:59:45 -0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
-export default {
+module.exports = {
   apps: [
     {
       name: "viv-final",
       script: "./src/server.js",
       watch: true,
-    },
+      interpreter: "node",
+      node_args: "--experimental-specifier-resolution=node"
+    }
   ],
 };
